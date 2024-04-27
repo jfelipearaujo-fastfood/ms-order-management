2c6cb098eb1d7a9c7dae5a72068fc685b802a8bc
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-04-27 00:44:06 -0300

    feat: :construction: add create order service

---
+++ internal/service/service.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"context"
+
+	"github.com/jfelipearaujo-org/ms-order-management/internal/common"
+	"github.com/jfelipearaujo-org/ms-order-management/internal/entity"
+)
+
+type CreateOrderDto struct {
+	CustomerID string `json:"customer_id"`
+}
+
+type CreateOrderService interface {
+	Handle(ctx context.Context, request CreateOrderDto) (*entity.Order, error)
+}
+
+type GetOrderDto struct {
+	UUID    string `param:"id"`       // api/v1/orders/:id
+	TrackID string `param:"track_id"` // api/v1/orders/tracking/:track_id
+}
+
+type GetOrderService interface {
+	Handle(ctx context.Context, request GetOrderDto) (entity.Order, error)
+}
+
+type GetOrdersDto struct {
+	State int `query:"state"`
+
+	common.Pagination
+}
+
+type GetOrdersService interface {
+	Handle(ctx context.Context, request GetOrdersDto) ([]entity.Order, error)
+}
+
+type UpdateOrderDto struct {
+	UUID string `param:"id"`
+
+	State int           `json:"state"`
+	Items []entity.Item `json:"items"`
+}
+
+type UpdateOrderService interface {
+	Handle(ctx context.Context, request UpdateOrderDto) (*entity.Order, error)
+}
